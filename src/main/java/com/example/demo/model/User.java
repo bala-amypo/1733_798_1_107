dd5bb2e6119511393fe0361f937d826a216a3f7e
bala-amypo <bala@amypo.in>
2025-12-18 05:07:49 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,8 +1,42 @@
-@Entity
-public class User{
+package com.example.demo.entity;
 
-private Long id;
-private String name;
-private String email;
-private String password;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(
+    name = "user",
+    uniqueConstraints = @UniqueConstraint(columnNames = "email")
+)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String password; 
+
+    @Column(nullable = false)
+    private String role;
+
+    
+    @PrePersist
+    public void setDefaultRole() {
+        if (this.role == null) {
+            this.role = "AGENT";
+        }
+    }
+
+    // Getters and Setters
 }
