6ceb56b19b305163638b3dc048e721a29c1c47f5
bala-amypo <bala@amypo.in>
2025-12-18 06:00:51 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/ParcelRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Parcel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParcelRepository extends JpaRepository<Parcel, Long> {
+
+    boolean existsByTrackingNumber(String trackingNumber);
+
+    Parcel findByTrackingNumber(String trackingNumber);
+}
