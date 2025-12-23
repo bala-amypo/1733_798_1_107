373688c48faf2b730515ffad355a05d449431f42
bala-amypo <bala@amypo.in>
2025-12-23 05:02:50 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/DamageClaimServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DamageClaimServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class ClaimRuleServiceImpl implements ClaimRuleService {
+public class DamageClaimServiceImpl implements DamageClaimService {
 
     private final ClaimRuleRepository ruleRepository;
 
