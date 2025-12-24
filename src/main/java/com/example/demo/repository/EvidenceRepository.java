9aa621ca7fddf5592feeea606f5f92b9dd2ffe46
bala-amypo <bala@amypo.in>
2025-12-24 03:59:49 +0000

    User commit

--- src/main/java/com/example/demo/repository/EvidenceRepository.java
+++ src/main/java/com/example/demo/repository/EvidenceRepository.java
@@ -3,13 +3,5 @@ package com.example.demo.repository;
 import com.example.demo.model.Evidence;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface EvidenceRepository extends JpaRepository<Evidence, Long> {
-
-    // ✅ REQUIRED by tests
-    long countByClaim_Id(Long claimId);
-
-    // ✅ REQUIRED by tests
-    List<Evidence> findByClaim_Id(Long claimId);
 }
