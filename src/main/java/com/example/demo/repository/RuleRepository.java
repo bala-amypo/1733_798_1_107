2306480aa69fdc7fab206fa3e4d30a31d0d642bb
bala-amypo <bala@amypo.in>
2025-12-24 05:53:58 +0000

    User commit

--- src/main/java/com/example/demo/repository/RuleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Rule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RuleRepository extends JpaRepository<Rule, Long> {
-}
--- src/main/java/com/example/demo/service/RuleService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.Rule;
-
-public interface RuleService {
-    Rule addRule(Rule rule);
-}
